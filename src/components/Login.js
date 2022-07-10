089973bde4e3f8933b6086d90780f7596c360c85
ivancard <ivaanlml@gmail.com>
2022-07-10 00:11:18 -0300

    login endpoint modified

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,7 @@ export const Login = () => {
         }
 
         axios
-            .post('https://challenge-react.alkemy.org', {
+            .post('http://challenge-react.alkemy.org', {
                 email,
                 password,
             })
